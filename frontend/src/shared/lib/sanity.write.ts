37579329bb651e2aab0d1b3bf24299a6d7252d35
Pavlo Tishynin <pashatishinin@gmail.com>
2026-07-08 19:39:58 +0200

    new

--- frontend/src/shared/lib/sanity.write.ts
+++ frontend/src/shared/lib/sanity.write.ts
@@ -3,10 +3,13 @@ import { apiVersion, dataset, projectId } from "./env";
 
 // console.log("Токен загружен:", !!process.env.SANITY_WRITE_TOKEN);
 // Этот клиент имеет права на запись
+
+const sanityToken =
+	process.env.SANITY_WRITE_TOKEN || (globalThis as any).env?.SANITY_WRITE_TOKEN;
 export const writeClient = createClient({
 	projectId,
 	dataset,
 	apiVersion,
 	useCdn: false,
-	token: process.env.SANITY_WRITE_TOKEN,
+	token: sanityToken,
 });
