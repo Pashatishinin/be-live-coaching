cc5bd2f947e378529496de22be0d31e24cdba790
Pavlo Tishynin <pashatishinin@gmail.com>
2026-07-09 10:27:54 +0200

    html resend

--- frontend/src/features/emails/sendAdminNotification.ts
+++ frontend/src/features/emails/sendAdminNotification.ts
@@ -1,17 +1,21 @@
+import { render } from "@react-email/render";
 import { Resend } from "resend";
 import AdminNotification from "./templates/AdminNotification";
 import type { SendEmailData } from "./types/types";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export const sendAdminNotification = async (payload: SendEmailData) => {
 	try {
+		const resend = new Resend(process.env.RESEND_API_KEY);
+
+		const html = await render(AdminNotification({ data: payload }));
+
 		const { data, error } = await resend.emails.send({
 			to: "tishyninpavlo@gmail.com",
 			// to: "juliasolodiuk@gmail.com",
 			from: "BLive Coaching <info@blivecoaching.com>",
 			subject: `Новая заявка: ${payload.applicantName}`,
-			react: AdminNotification({ data: payload }),
+			// react: AdminNotification({ data: payload }),
+			html,
 		});
 
 		if (error) {
--- frontend/src/features/emails/sendApplicantConfirmation.ts
+++ frontend/src/features/emails/sendApplicantConfirmation.ts
@@ -1,16 +1,19 @@
+import { render } from "@react-email/render";
 import { Resend } from "resend";
 import ApplicationNotification from "./templates/ApplicationNotification";
 import type { SendEmailData } from "./types/types";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export const sendApplicantConfirmation = async (payload: SendEmailData) => {
 	try {
+		const resend = new Resend(process.env.RESEND_API_KEY);
+
+		const html = await render(ApplicationNotification({ data: payload }));
+
 		const { data, error } = await resend.emails.send({
 			to: payload.applicantEmail,
 			from: "BLive Coaching <info@blivecoaching.com>",
 			subject: "Ми отримали вашу заявку! | BLive Coaching",
-			react: ApplicationNotification({ data: payload }),
+			html,
 		});
 
 		if (error) {
